570f666999fc7a5c0b91d93fd8b4ff6241cdc6db
5170 <maksymilianguzek1@gmail.com>
2021-12-30 12:26:47 +0100

    push

---
+++ src/main/java/dev/isnow/fox/util/reach/reach/GetBoundingBox.java
@@ -0,0 +1,15 @@
+package dev.isnow.fox.util.reach.reach;
+
+import dev.isnow.fox.util.reach.reach.SimpleCollisionBox;
+
+public class GetBoundingBox {
+    public static SimpleCollisionBox getBoundingBoxFromPosAndSize(double centerX, double minY, double centerZ, double width, double height) {
+        double minX = centerX - (width / 2);
+        double maxX = centerX + (width / 2);
+        double maxY = minY + height;
+        double minZ = centerZ - (width / 2);
+        double maxZ = centerZ + (width / 2);
+
+        return new SimpleCollisionBox(minX, minY, minZ, maxX, maxY, maxZ, false);
+    }
+}
