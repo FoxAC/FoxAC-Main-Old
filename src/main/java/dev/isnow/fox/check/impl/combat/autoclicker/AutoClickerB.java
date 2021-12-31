64c36d24aec38ed86d7016ad508b97ec710a0a04
ryand <daviesryan35@gmail.com>
2021-12-31 06:14:17 +0000

    New autoclicker and exploit checks

--- src/main/java/dev/isnow/fox/check/impl/combat/autoclicker/AutoClickerB.java
+++ src/main/java/dev/isnow/fox/check/impl/combat/autoclicker/AutoClickerB.java
@@ -6,40 +6,46 @@ import dev.isnow.fox.check.api.CheckInfo;
 import dev.isnow.fox.data.PlayerData;
 import dev.isnow.fox.exempt.type.ExemptType;
 import dev.isnow.fox.packet.Packet;
+import dev.isnow.fox.util.MathUtil;
+import dev.isnow.fox.util.type.EvictingList;
 
 @CheckInfo(name = "AutoClicker", type = "B", description = "Checks the outliers on your clicks")
 public class AutoClickerB extends Check {
-    private int outliers;
-    private int lastoutlier;
-    private float threshold;
-    private int flying;
 
-    public AutoClickerB(PlayerData data) {
+    private final EvictingList<Long> tickList = new EvictingList<>(30);
+    private double lastDeviation;
+    private int tick;
+
+    public AutoClickerB(final PlayerData data) {
         super(data);
     }
 
     @Override
-    public void handle(Packet packet) {
-        if (packet.isArmAnimation() && !isExempt(ExemptType.AUTOCLICKER) && flying != 0) {
+    public void handle(final Packet packet) {
+        if (packet.isArmAnimation()) {
+            final boolean exempt = isExempt(ExemptType.DROP, ExemptType.AUTOCLICKER);
+            if (!exempt) tickList.add((long) (tick * 50.0));
 
-            if (flying > 3 && flying < 10) {
-                outliers++;
-            }
+            if (tickList.isFull()) {
+                final double deviation = MathUtil.getStandardDeviation(tickList);
+                final double difference = Math.abs(deviation - lastDeviation);
 
-            int currentOutliers = outliers;
-            int diff = Math.abs(currentOutliers - lastoutlier);
-            if (currentOutliers == 0 && diff == currentOutliers) {
-                if (threshold > 32) {
-                    fail("diff=" + diff + ", threshold=" + threshold + ", cps=" + data.getClickProcessor().getCps());
+                final boolean invalid = difference < 6;
+
+                debug("diff: " +deviation+ "devi: " +deviation);
+
+                if (invalid && !exempt) {
+                    if (increaseBuffer() > 15) {
+                        fail(deviation);
+                    }
+                } else {
+                    decreaseBuffer();
                 }
-                threshold += 0.10f;
-            } else {
-                threshold = 0;
+
+                lastDeviation = deviation;
             }
-            lastoutlier = currentOutliers;
-            outliers = flying = 0;
         } else if (packet.isFlying()) {
-            flying++;
+            tick++;
         }
     }
 }
